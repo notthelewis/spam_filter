e079ee2832e134d84dbd04de8268cbfa9ec0d7f4
Lewis <43108320+notthelewis@users.noreply.github.com>
2020-03-17 15:57:30 +0000

    Update get_list.sh

--- get_list.sh
+++ get_list.sh
@@ -10,3 +10,5 @@ nano english_swears # Manually clean up
 cat english_swears | sed 's/<li><a href="\/wiki\///' > english_swears_semi_clean
 cat english_swears_semi_clean| sed 's/".*//' > english_swears_mostly_clean
 cat english_swears_mostly_clean | sed 's/<ul>//' > english_swears_clean
+cat english_swears_clean | sed -e 's/_/ /g' -e 's/-/ /' > cleaner-english-swears
+cat cleaner-english-swears | sed "s/\%27/'/" > clean-english-swears
