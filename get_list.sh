51f13f8e3cece79c52d11ad76bffb538437d0b89
Lewis <43108320+notthelewis@users.noreply.github.com>
2020-03-17 17:38:14 +0000

    Update get_list.sh

--- get_list.sh
+++ get_list.sh
@@ -5,3 +5,5 @@ cat Category\:English_swear_words | grep 'a href="/wiki' > references
 # Manually remove anything which is not a category, then proceed to run the following:
 cat references | sed -e 's/<[^>]*>//g' > all_categories
 # ^ Removes all HTML tags.
+cat all_categories| sed 's/^$*/https:\/\/en\.wiktionary\.org\/wiki\//' > category_links
+# ^ Converts the categories to links, still needs manual intervention.
